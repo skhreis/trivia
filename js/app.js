f3872ce74735d9f4ea9663d9710f7ca9ad628465
skhreis <skhreis0@gmail.com>
2022-04-10 15:31:26 -0400

    Added answer checking and a few button clicks.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 // ===  Variables  ===
-let question = document.querySelector('.question')
-let qns = document.querySelector('.qnumscore')
-let a = document.querySelector('.opt1')
-let b = document.querySelector('.opt2')
-let c = document.querySelector('.opt3')
-let d = document.querySelector('.opt4')
-arr = [a,b,c,d]
-
+const question = document.querySelector('.question')
+const qns = document.querySelector('.qnumscore')
+const a = document.querySelector('.opt1')
+const b = document.querySelector('.opt2')
+const c = document.querySelector('.opt3')
+const d = document.querySelector('.opt4')
+let arr = []
+let answer = ''
+let choice = ''
+let click = false
 
 // ===  Question and Answer Functionality  === 
 let questQ = [
@@ -19,37 +21,100 @@ let questQ = [
 	['What is "cynophobia"?', 'Fear of Dogs', 'Fear of Animals', 'Fear of Cryo', 'Fear of Cyno'],
 	['What punctuation mark ends an imperative sentence?', 'Period', 'Question Mark', 'Exclamation Point', 'Comma'],
 	['Who named the Pacific Ocean?', 'Christopher Columbus', 'Ferdinand Magellan', "Vasco da Gama's", 'Prince Vijaya'],
-	['How many languages are written from right to left?', '9', '10', '11', '12'],
+	['How many languages are written from right to left?', '11', '9', '12', '10'],
 	['How many countries still have Shilling as currency?', 'One', 'Two', 'Three', 'Four'],
 	['What is the name of the man who launched eBay back in 1995?', 'Pierre Omidyar', 'Jeff Bezos', 'Bill Gates', 'Nick Huzar'],
 	['What is the name of the biggest technology company in South Korea?', 'Apple', 'Sony', 'Samsung', 'Nintendo'],
 	['Which animal can be seen on the Porsche logo?', 'Donkey', 'Horse', 'Llama', 'Cheetah'],
 	["Which monarch officially made Valentine's Day a holiday in 1537?", 'Henry VIII', 'William IV', 'George I', 'Victoria']
 ]
-let q0Opt = ['Microsoft', 'David Kelly', 'IBM', ' David Eddy'] // David Eddy
-let q1Opt = ['World Web Wide', 'World Wide Web', 'Web World Wide', 'Width of World Web'] // World Wide Web
-let q2Opt = ['25 Meters', '50 Meters', '75 Meters', '100 Meters'] // 50 Meters
-let q3Opt = ['Italy, France, Austria', 'Germany, Austria, Poland', 'Japan, China, Germany', 'Italy, Germany, Japan'] // Italy, Germany, Japan
-let q4Opt = ['New Zealand', 'Australia', 'Indonesia', 'Great Britan'] // Australia
-let q5Opt = ['Hexagon', 'Heptagon', 'Enneagon', 'Octogon'] // Octogon
-let q6Opt = ['Fear of Dogs', 'Fear of Animals', 'Fear of Cryo', 'Fear of Cyno'] // Fear of Dogs
-let q7Opt = ['Period', 'Question Mark', 'Exclamation Point', 'Comma'] // Exclamation Point
-let q8Opt = ['Christopher Columbus', 'Ferdinand Magellan', "Vasco da Gama's", 'Prince Vijaya'] // Ferdinand Magellan
-let q9Opt = ['9', '10', '11', '12'] // 12
-let q10Opt = ['One', 'Two', 'Three', 'Four'] // Four
-let q11Opt = ['Pierre Omidyar', 'Jeff Bezos', 'Bill Gates', 'Nick Huzar'] // Pierre Omidyar
-let q12Opt = ['Apple', 'Sony', 'Samsung', 'Nintendo'] // Samsung
-let q13Opt = ['Donkey', 'Horse', 'Llama', 'Cheetah'] // Horse
-let q14Opt = ['Henry VIII', 'William IV', 'George I', 'Victoria'] // Henry VIII
+// let q0Opt = ['Microsoft', 'David Kelly', 'IBM', ' David Eddy'] // David Eddy d
+// let q1Opt = ['World Web Wide', 'World Wide Web', 'Web World Wide', 'Width of World Web'] // World Wide Web a
+// let q2Opt = ['25 Meters', '50 Meters', '75 Meters', '100 Meters'] // 50 Meters b
+// let q3Opt = ['Italy, France, Austria', 'Germany, Austria, Poland', 'Japan, China, Germany', 'Italy, Germany, Japan'] // Italy, Germany, Japan d
+// let q4Opt = ['New Zealand', 'Australia', 'Indonesia', 'Great Britan'] // Australia b
+// let q5Opt = ['Hexagon', 'Heptagon', 'Enneagon', 'Octogon'] // Octogon d
+// let q6Opt = ['Fear of Dogs', 'Fear of Animals', 'Fear of Cryo', 'Fear of Cyno'] // Fear of Dogs a
+// let q7Opt = ['Period', 'Question Mark', 'Exclamation Point', 'Comma'] // Exclamation Point c
+// let q8Opt = ['Christopher Columbus', 'Ferdinand Magellan', "Vasco da Gama's", 'Prince Vijaya'] // Ferdinand Magellan b
+// let q9Opt = ['11', '9', '12', '10'] // 12 c
+// let q10Opt = ['One', 'Two', 'Three', 'Four'] // Four d
+// let q11Opt = ['Pierre Omidyar', 'Jeff Bezos', 'Bill Gates', 'Nick Huzar'] // Pierre Omidyar a
+// let q12Opt = ['Apple', 'Sony', 'Samsung', 'Nintendo'] // Samsung c
+// let q13Opt = ['Donkey', 'Horse', 'Llama', 'Cheetah'] // Horse b
+// let q14Opt = ['Henry VIII', 'William IV', 'George I', 'Victoria'] // Henry VIII a
+
+// a = 1,6,11,14
+// b = 2,4,8,13
+// c = 7,9,12
+// d = 0,3,5,10
 
 function generateQ() {
 	let qChoice = Math.round(Math.random() * (14.49 + 0.5) - 0.5)
+	for(let i = 0; i<10; i++){
+		if(qChoice === arr[i]) {
+			console.log('Same number occurred, regenerating')
+			qChoice = Math.round(Math.random() * (14.49 + 0.5) - 0.5)
+		}
+	}
+	arr.push(qChoice)
+	console.log(arr)
+	
+	if(qChoice === 1 || qChoice === 6 || qChoice === 11 || qChoice === 14) {
+		answer = 'A'
+	} else if(qChoice === 2 || qChoice === 4 || qChoice === 8 || qChoice === 13) {
+		answer = 'B'
+	} else if (qChoice === 7 || qChoice === 9 || qChoice === 12) {
+		answer = 'C'
+	} else answer = 'D'
+	// console.log(qChoice)
+	console.log(answer)
 	question.innerHTML = questQ[qChoice][0]
-	console.log(qChoice)
 	a.innerText = `A - ${questQ[qChoice][1]}`
 	b.innerText = `B - ${questQ[qChoice][2]}`
 	c.innerText = `C - ${questQ[qChoice][3]}`
 	d.innerText = `D - ${questQ[qChoice][4]}`
+	click = false
 }
 generateQ()
-console.log(questQ[0][1])
+
+function checkAnswer() {
+	click = true
+	if(choice === answer) {
+		console.log('You are Correct!')
+	} else console.log('You are Incorrect!')
+	setTimeout(generateQ, 5000)
+}
+
+
+a.addEventListener('click', () => {
+	if(click === false){
+		choice = 'A'
+		click = true
+		checkAnswer()
+	} else console.log('Please wait to click again...')
+})
+
+b.addEventListener('click', () => {
+	if(click === false){
+		choice = 'B'
+		click = true
+		checkAnswer()
+	} else console.log('Please wait to click again...')
+})
+
+c.addEventListener('click', () => {
+	if(click === false){
+		choice = 'C'
+		click = true
+		checkAnswer()
+	} else console.log('Please wait to click again...')
+})
+
+d.addEventListener('click', () => {
+	if(click === false){
+		choice = 'D'
+		click = true
+		checkAnswer()
+	} else console.log('Please wait to click again...')
+})
